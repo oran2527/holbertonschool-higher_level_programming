05b7ea0f0d87620c8c12e4c6bcd7dab9ae2b5240
oran2527 <orlago250183@gmail.com>
2019-12-11 01:23:14 +0000

    point 1 print less greater equal change print

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0	
+if number > 0:	
 	l = number % 10
 else:
 	l = abs(number) % 10 * -1
+print("Last digit of {:d} is {:d} and is ".format(number, l),end="");
 if l > 5:
-	print("Last digit of {:d} is {:d} and is greater than 5".format(number, l))
+	print(" greater than 5")
 elif l < 6 and l != 0:
-	print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, l))
+	print("less than 6 and not 0")
 else:
-	print("Last digit of {:d} is {:d} and is 0".format(number, l))
+	print("0".format(number, l))
