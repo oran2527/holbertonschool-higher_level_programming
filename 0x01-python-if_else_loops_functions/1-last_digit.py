48e5c13ef8e6648cccc0368989e0d721a98c0a1b
oran2527 <orlago250183@gmail.com>
2019-12-11 01:25:42 +0000

    point 1 print less greater equal change print

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,11 @@ if number > 0:
 	l = number % 10
 else:
 	l = abs(number) % 10 * -1
-print("Last digit of {:d} is {:d} and is ".format(number, l),end="");
+
+print("Last digit of {:d} is {:d} ".format(number, l),end="");
 if l > 5:
-	print(" greater than 5")
+	print("and is greater than 5")
 elif l < 6 and l != 0:
-	print("less than 6 and not 0")
+	print("and is less than 6 and not 0")
 else:
-	print("0".format(number, l))
+	print("and is 0".format(number, l))
