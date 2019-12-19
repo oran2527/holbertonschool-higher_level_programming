e34ca1b72170e03e5af1ab98ea656774bed49116
oran2527 <orlago250183@gmail.com>
2019-12-19 21:12:15 +0000

    point 7 update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary is not None:
+        for i in a_dictionary.copy():
+            if i == key:
+                a_dictionary.update({i: value})
+            else:
+                a_dictionary.update({key: value})
+    return a_dictionary
