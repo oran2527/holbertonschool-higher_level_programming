8dd755f78c8ae9e721152d4d307788f6516c9f3b
oran2527 <orlago250183@gmail.com>
2019-12-20 02:31:37 +0000

    point 12 roman numbers

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+def roman(i):
+    switcher = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+         }
+    return switcher.get(i, 0)
+
+
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    new = []
+    total = 0
+    flag = 0
+    for i in roman_string:
+        if i.isdigit() is True:
+            return 0
+        else:
+            new.append(roman(i))
+    for i in range(0, len(new)):
+        if i < (len(new) - 1):
+            if (new[i + 1] == 5 or new[i + 1] == 10 and flag == 0):
+                if new[i] == 1 and new[i] < new[i + 1]:
+                    total += new[i] * -1
+                    flag = 1
+            if new[i + 1] == 50 or new[i + 1] == 100 and flag == 0:
+                if new[i] == 10 and new[i] < new[i + 1]:
+                    total += new[i] * -1
+                    flag = 1
+            if new[i + 1] == 500 or new[i + 1] == 1000 and flag == 0:
+                if new[i] == 100 and new[i] < new[i + 1]:
+                    total += new[i] * -1
+                    flag = 1
+            if flag == 0:
+                total += new[i]
+        else:
+            total += new[i]
+    return total
