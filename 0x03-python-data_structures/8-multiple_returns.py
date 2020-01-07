826313e359a8a3f52935b0411f6256e7c1a2377f
oran2527 <orlago250183@gmail.com>
2020-01-07 03:24:16 +0000

    point 8 return tuple multiple values

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence is None:
+        length = len(sentence)
+        first = None
+    else:
+        length = len(sentence)
+        first = sentence[0]
+    return length, first
