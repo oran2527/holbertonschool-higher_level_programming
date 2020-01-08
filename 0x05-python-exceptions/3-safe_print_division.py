33c0d15bb34512aa69c9c4f2f9377d876dbf6bb4
oran2527 <orlago250183@gmail.com>
2020-01-08 17:04:16 +0000

    point 3 pep8

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,17 +2,17 @@
 def safe_print_division(a, b):
     import sys
     i = 0
-    flag = False	
+    flag = False
 
     try:
         flag = False
         i = a / b
         return i
-    except ZeroDivisionError:        
+    except ZeroDivisionError:
         flag = True
         i = 0
     finally:
-        if  flag == True: 
+        if flag is True:
             print("Inside result: {}".format(None))
         else:
             print("Inside result: {:1.1f}".format(i))
