77700658338dda54d97d241e762e1013132465b0
oran2527 <orlago250183@gmail.com>
2020-01-08 17:18:13 +0000

    point 1 conditional is

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if int(value) - int(value) == 0:
+        if int(value) - int(value) is 0:
             print("{:d}".format(int(value)))
             return True
     except ValueError:
