e6c7881097359be0b44dd68b0e60aeddcc5ffcbc
oran2527 <orlago250183@gmail.com>
2020-01-08 17:20:27 +0000

    point 1 delete conditional and cast int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if int(value) - int(value) is 0:
-            print("{:d}".format(int(value)))
-            return True
+        print("{:d}".format(value))
+        return True
     except ValueError:
         return False
