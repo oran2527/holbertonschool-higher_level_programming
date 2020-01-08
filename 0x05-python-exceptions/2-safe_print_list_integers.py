8496126ea68bbae8f220c3c1b1d647b70688e340
oran2527 <orlago250183@gmail.com>
2020-01-08 17:11:36 +0000

    point 2 pep8

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    import sys
-    cont = 0
-
     try:
+        cont = 0
         for x in range(0, x):
             if type(my_list[x]) == int:
                 print(my_list[x], end="")
