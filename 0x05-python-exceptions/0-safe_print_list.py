06cd626e3825505554fca1950790a15b43e4d47d
oran2527 <orlago250183@gmail.com>
2020-01-08 17:50:51 +0000

    point 0 delete error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[x], end="")
         print("\n", end="")
         return x + 1
-    except IndexError:
+    except:
         i = 0
         print("\n", end="")
         for x in my_list:
