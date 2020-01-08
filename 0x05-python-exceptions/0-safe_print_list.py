ba0057ad70d5d7ea830a4745fe176cef84605812
oran2527 <orlago250183@gmail.com>
2020-01-08 17:09:03 +0000

    point 0 pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    import sys
     try:
         for x in range(0, x):
             print(my_list[x], end="")
-        print("\n", end="")    
+        print("\n", end="")
         return x + 1
     except IndexError:
         i = 0
