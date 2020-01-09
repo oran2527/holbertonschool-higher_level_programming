fe0f7452e63c733ca4081c55422806b2231e92e0
oran2527 <orlago250183@gmail.com>
2020-01-09 01:59:39 +0000

    point 8 safe function stderr import sys exception unification

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,9 +5,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(args[0], args[1])
-    except ZeroDivisionError as zero_err:
-        print("Exception: " + zero_err.args[0], file=sys.stderr)
-        return None
-    except IndexError as ind_err:
-        print("Exception: " + ind_err.args[0], file=sys.stderr)
-        return None
+    except Exception as err:
+        print("Exception: " + err.args[0], file=sys.stderr)
+        return None  
