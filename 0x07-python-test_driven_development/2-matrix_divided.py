4354384e0163b409608bd91485cde77e401d96d9
oran2527 <orlago250183@gmail.com>
2020-01-12 22:09:40 +0000

    point 0

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""matrix_divided - div of list of list by an element
+
+   Description: return a list of list with the division by element
+   Return: list of list
+"""
+
+
+def def matrix_divided(matrix, div):
+    """matrix_divided - function.
+       matrix - first value.
+       div - second value."""
+    new = []
+    if div is 0:
+        raise ZeroDivisionError("division by zero")
+    return new
