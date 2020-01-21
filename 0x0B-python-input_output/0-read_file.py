091714d515fdaed5395a008db8c466e0d04b8041
oran2527 <orlago250183@gmail.com>
2020-01-21 15:52:38 +0000

    point 0 fix end betty

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,9 +4,8 @@
     Description: Read a file
     Return: content of a file"""
 
-    
+
 def read_file(filename=""):
     """read_file function"""
     with open(filename, 'r') as f:
         print(f.read(), end="")
-    
