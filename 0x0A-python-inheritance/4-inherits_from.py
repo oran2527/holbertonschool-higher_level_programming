b9e6a0e6cc31d436c2a90d125f62e0126469e62c
oran2527 <orlago250183@gmail.com>
2020-01-21 00:45:09 +0000

    point 6 and 7

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,7 +10,7 @@ def inherits_from(obj, a_class):
     """inherits_from - function.
        obj - object.
        a_class."""
-    if issubclass(type(obj), a_class) and type(obj) is not a_class:	  
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         return False
