0b7d4de20dc390b72d12254ec4952ccb7ba40dce
oran2527 <orlago250183@gmail.com>
2020-01-21 16:12:46 +0000

    point 1 number lines

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module point 0
+
+    Description: Count the numbers of lines of a file
+    Return: number lines"""
+
+
+def number_of_lines(filename=""):
+    """number_of_lines"""
+    cont = 1
+    with open(filename, 'r') as f:
+        for line in f:
+            if line.strip():
+                cont += 1
+    return cont
