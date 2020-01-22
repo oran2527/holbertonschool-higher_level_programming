0fa525d3f7c2fbeae9a6f3cd4ca1422b89645d80
oran2527 <orlago250183@gmail.com>
2020-01-22 02:26:43 +0000

    point 10 fix

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 import json
 """ Module point 10
-
-    Description: Class to Json
-    Return: data structure"""
-
+"""
 
 def class_to_json(obj):
     """class_to_json function"""
