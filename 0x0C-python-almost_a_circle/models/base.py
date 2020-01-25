8e11ca663a485d197a232e7db93a59398456aa18
oran2527 <orlago250183@gmail.com>
2020-01-25 02:48:20 +0000

    point 1 base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/user/bin/python3
+""" module point 1 base class """
+
+
+class Base:
+    """class Base"""
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """init function"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
