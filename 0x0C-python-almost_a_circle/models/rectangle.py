88fa32c5d62ad650156e6af2c219ba48b7cd137f
oran2527 <orlago250183@gmail.com>
2020-01-27 19:13:14 +0000

    erase character >

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -103,7 +103,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """str function"""
-        return "[{}] ({}) {}/{} - {}/{}>\
+        return "[{}] ({}) {}/{} - {}/{}\
 ".format(type(self).__name__, self.id, self.x, self.y,
          self.width, self.height)
 
