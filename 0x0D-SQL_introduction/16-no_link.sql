fac99fe6e21743f7feb29ba9ce747a916aca4205
oran2527 <orlago250183@gmail.com>
2020-03-02 18:49:39 +0000

    point 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- point 16
 -- Dont list rows without a name value
-SELECT score AS score, name AS name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
