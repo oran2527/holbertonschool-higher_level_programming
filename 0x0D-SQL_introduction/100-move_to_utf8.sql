767141c75dcac35958b524e7ab4ec906c1c3456c
oran2527 <orlago250183@gmail.com>
2020-03-02 19:10:18 +0000

    fix mb4

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- point 17
 -- convert database to utf8mb4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
