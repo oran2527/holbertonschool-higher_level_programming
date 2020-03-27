460af7e294b0e3073861c8ae58653e4b4b7b6c99
oran2527 <orlago250183@gmail.com>
2020-03-27 22:27:06 +0000

    point 2 names starting with N

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Task 2 display states according argument '''
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    '''import mysqldb'''
+
+    import sys
+    '''import sys for arguments'''
+
+    db_connection = MySQLdb.Connect(host="localhost\
+", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id;")
+    myresult = cursor.fetchall()
+    for x in myresult:
+        print(x)
+    cursor.close()
+    db_connection.close()
