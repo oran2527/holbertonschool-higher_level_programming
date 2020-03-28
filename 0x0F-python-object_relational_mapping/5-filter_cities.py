4e2a1b4bb35ffeafb6357091a0036eb4443b0d55
oran2527 <orlago250183@gmail.com>
2020-03-28 03:00:12 +0000

    point 5 cities from a state with argument

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''Task 5 display all cities from a state according argument '''
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    '''import mysqldb'''
+
+    import sys
+    '''import sys for arguments'''
+
+    db_connection = MySQLdb.Connect(host="localhost\
+", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db_connection.cursor()
+    cursorstr = """
+SELECT name
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = '{value}')
+ORDER BY cities.id;
+"""
+    value = str(sys.argv[4])
+    cursorstr = cursorstr.format(value=value)
+    cursor.execute(cursorstr)
+    myresult = cursor.fetchall()
+    for y in range(0, len(myresult)):
+        w = str(myresult[y])
+        x = str(w.replace(",", "").replace("(", "").replace(")\
+", "").replace("'", ""))
+        print(x, end="")
+        if y != len(myresult) - 1:
+            print(", ", end="")
+        else:
+            print()
+    cursor.close()
+    db_connection.close()
