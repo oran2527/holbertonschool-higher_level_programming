c9ff5a897a37df428caafb330457dbc6689d3507
oran2527 <orlago250183@gmail.com>
2020-03-28 17:59:33 +0000

    point 10 state with argument pep8

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     records = ses.query(State).filter(State.name == s4).all()
     if records:
-        for record in records:    
+        for record in records:
             print("{}".format(record.id))
     else:
         print("Not found")
