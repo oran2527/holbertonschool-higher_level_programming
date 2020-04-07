1a87a40393bb12c5d315d7697de246fd4bf8b679
oran2527 <orlago250183@gmail.com>
2020-04-07 16:56:42 +0000

    point 6 python sort list find peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' point 6 find a peak in a list '''
+
+def find_peak(list_of_integers):
+    ''' function to find the max value in a list '''
+    end = len(list_of_integers)    
+    list_of_integers.sort(reverse=False)
+    return list_of_integers[end - 1]	
