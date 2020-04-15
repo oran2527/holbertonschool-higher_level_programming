5af4f98084be0844a8ff34b537925e51ff950c7f
oran2527 <orlago250183@gmail.com>
2020-04-15 19:20:27 +0000

    point 0 tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         data = response.read()
         print("Body response:")
-        print("    - type: {}".format(type(data)))
-        print("    - content: {}".format(data))
-        print("    - utf8 content: {}".format(data.decode('utf-8')))
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('utf-8')))
