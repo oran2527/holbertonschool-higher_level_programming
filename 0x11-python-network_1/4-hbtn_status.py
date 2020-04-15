fcaf247cb166cc749c88b1c166d80badfe451377
oran2527 <orlago250183@gmail.com>
2020-04-15 19:14:00 +0000

    point 4 request fetch

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' point 4 status requests '''
+
+if __name__ == "__main__":
+
+    import requests
+
+    response = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("    - type: {}".format(type(response.text)))
+    print("    - content: {}".format(response.text))
