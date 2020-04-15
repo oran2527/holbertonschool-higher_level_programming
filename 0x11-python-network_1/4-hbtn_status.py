5e7c0d2e0a0f72699c50ecfb5e4d998b104413f0
oran2527 <orlago250183@gmail.com>
2020-04-15 19:19:25 +0000

    point 4 request fetch single quotes tab

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 ''' point 4 status requests '''
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     import requests
 
     response = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("    - type: {}".format(type(response.text)))
-    print("    - content: {}".format(response.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
