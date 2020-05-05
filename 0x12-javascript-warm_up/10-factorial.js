5131fa9e364a83ca251465fc7aba9fc282e367aa
oran2527 <orlago250183@gmail.com>
2020-05-05 21:16:24 +0000

    point 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
-function fact (a, b) {
-  console.log(a + b)
+let res = 1
+function fac (res, arg) {
+  if (arg > 1) {
+    res *= arg;
+    fac (res, arg - 1);
+  }
+  else {
+    console.log(res);
+  };
 };
-
-add (parseInt(process.argv[2]), parseInt(process.argv[3]));
+fac (res, parseInt(process.argv[2]));
