88d780cc57e9d3090cca8701bc8d290232291416
oran2527 <orlago250183@gmail.com>
2020-05-05 23:11:42 +0000

    point 6 semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let ar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (i = 0; i < ar.length; i++) { 
-  console.log(ar[i]); 
-}; 
+const ar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < ar.length; i++) {
+  console.log(ar[i]);
+}
