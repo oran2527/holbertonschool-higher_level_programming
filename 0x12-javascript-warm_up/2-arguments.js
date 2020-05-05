e51f00eecefc826c38e985213548c1c860afacb3
oran2527 <orlago250183@gmail.com>
2020-05-05 17:32:03 +0000

    point 2 change commands

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
-let args = 0;
-process.argv.forEach(() => {
-  args += 1;
-});
-if (args === 2) {
-  console.log('No argument');
-};
-if (args === 3) {
-  console.log('Argument found');
-};
-if (args > 3) {
+if (process.argv3]) {
   console.log('Arguments found');
-};
-
+} else if (process.argv[2]) {
+  console.log('Argument found');
+} else {
+  console.log('No arguments');
+}
