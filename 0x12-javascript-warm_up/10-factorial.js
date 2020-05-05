56a1cd45bd44ea2e3f286c5f038e50769eb2b036
oran2527 <orlago250183@gmail.com>
2020-05-05 20:53:45 +0000

    point 13

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+function fact (a, b) {
+  console.log(a + b)
+};
+
+add (parseInt(process.argv[2]), parseInt(process.argv[3]));
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+module.exports = {
+  add: function add (a, b) {
+    var sum = a + b;
+    return sum;
+  }
+};  
