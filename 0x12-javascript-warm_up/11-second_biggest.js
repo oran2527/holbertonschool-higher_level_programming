327cd917486233df00baae03402cf51a0307bd9a
oran2527 <orlago250183@gmail.com>
2020-05-05 23:43:54 +0000

    point 11 semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
-let ar = []
-let ind = 0
+const ar = [];
+let ind = 0;
 if (process.argv[2] === undefined || process.argv[3] === undefined) {
   console.log(0);
 } else {
-  process.argv.forEach(function (val, index) {     
+  process.argv.forEach(function (val, index) {
     if (index > 1) {
       ar[ind] = process.argv[index];
       ind += 1;
-    };
+    }
   });
-  ar.sort(function (a, b){
+  ar.sort(function (a, b) {
     return a - b;
   });
   console.log(ar[ar.length - 2]);
-}; 
+}
