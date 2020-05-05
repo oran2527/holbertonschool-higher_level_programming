ced21de1ac17116327414b4cdd5428b3ff318a62
oran2527 <orlago250183@gmail.com>
2020-05-05 16:24:14 +0000

    point 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+let args = 0
+process.argv.forEach(() => {
+  args += 1;
+});
+if (args === 2) {
+  console.log('No argument')
+}
+if (args === 3) {
+  console.log('Argument found')
+}
+if (args > 3) {
+  console.log('Arguments found')
+}
+
