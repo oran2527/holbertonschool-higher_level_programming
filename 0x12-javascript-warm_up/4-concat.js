c0f7f083d82a5715c778063ed5e9b1e42ef7aeac
oran2527 <orlago250183@gmail.com>
2020-05-05 23:02:02 +0000

    point 4 semistandard

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-
-let args = 0
-let text1
-let text2
+let text1;
+let text2;
 process.argv.forEach((val, index) => {
-  args += 1
   if (index === 2) {
-    text1 = val    
+    text1 = val;
   }
   if (index === 3) {
-    text2 = val    
+    text2 = val;
   }
 });
-console.log(text1 + ' is ' + text2)
+console.log(text1 + ' is ' + text2);
