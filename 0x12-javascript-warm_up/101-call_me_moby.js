9c8f639b4d538c8b73797e2f657abea85a9933cd
oran2527 <orlago250183@gmail.com>
2020-05-05 23:50:49 +0000

    point 15 semistandard

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 module.exports = {
   callMeMoby: function (x, theFunction) {
-    for (i = 0; i < parseInt(x); i++) {
+    for (let i = 0; i < parseInt(x); i++) {
       theFunction();
-    }  
+    }
   }
-};  
+};
