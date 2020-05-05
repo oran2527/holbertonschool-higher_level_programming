5b2fe8d053f7feeb5644f6d3816cc142681378c3
oran2527 <orlago250183@gmail.com>
2020-05-05 17:26:26 +0000

    point 2 semicolons

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,11 +5,11 @@ process.argv.forEach(() => {
 });
 if (args === 2) {
   console.log('No argument');
-}
+};
 if (args === 3) {
   console.log('Argument found');
-}
+};
 if (args > 3) {
   console.log('Arguments found');
-}
+};
 
