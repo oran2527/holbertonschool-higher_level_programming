51a0713a6bf8c6b321b920b05bb8578f62e54745
oran2527 <orlago250183@gmail.com>
2020-05-11 19:52:09 +0000

    point 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,9 +4,9 @@ const Rectangle = require('./4-rectangle.js');
 class Square extends Rectangle {
   constructor (size) {
     if (size > 0) {
-      super(size, size);
-      this.width = size;
-      this.height = size;
+      super();
+      super.width = size;
+      super.height = size;
     }
   }
 }
