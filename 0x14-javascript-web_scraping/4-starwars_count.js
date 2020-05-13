c6a4a07862e9dc9134ef9be6718270fa860a71cd
oran2527 <orlago250183@gmail.com>
2020-05-13 00:48:01 +0000

    point 7 name character

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ request(url, function (error, response) {
   const info = JSON.parse(response.body);
   for (let i = 0; i < info.count; i++) {
     for (let j = 0; j < info.results[i].characters.length; j++) {
-      if (info.results[i].characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
+      if (info.results[i].characters[j] === 'https://swapi-api.hbtn.io/api/people/18') {
         count++;
       }
     }
