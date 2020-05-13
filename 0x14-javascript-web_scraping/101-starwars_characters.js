c7bc7e6c7991c84c36151b6b8ae47af7f9c11f41
oran2527 <orlago250183@gmail.com>
2020-05-13 01:24:25 +0000

    point 4 /

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
 const request2 = require('request');
+let list = [];
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '?format=json';
-request(url, function (error, response) {
+request(url, function (error, response) {  
   if (error) throw error;
   const info = JSON.parse(response.body);
   for (const i in info.characters) {
     request2(info.characters[i], function (error2, response2) {
       if (error2) throw error2;
-      const info2 = JSON.parse(response2.body);
-      console.log(info2.name);
+      const info2 = JSON.parse(response2.body);      
+      list[list.length] = info2.name;            
     });
-  }
+  }  
 });
+
+
